405bb294f8d747ee5ccb478a37402e7e4958a123
daveknussman <dknussman@yahoo.com>
2019-09-25 20:04:38 -0400

    exercise 3

--- exercises/03-intro-challenges/intro-challenges.js
+++ exercises/03-intro-challenges/intro-challenges.js
@@ -8,7 +8,19 @@
  * countNumberOfDigits(123); // 3
  * countNumberOfDigits(1000); // 4
  */
-function countNumberOfDigits(num) {}
+// npm run test test/03-intro-challenges/intro-challenges-test.js
+function countNumberOfDigits(num) {
+  let count = 0;
+  
+  if (num !== 0) ++count;
+
+while (num / 10 >= 1) {
+  num /= 10;
+  ++count;
+}
+
+return count;
+}
 
 /**
  * Given a number of seconds, return how long ago something happened.
@@ -24,7 +36,13 @@ function countNumberOfDigits(num) {}
  * getRelativeTime(59); // seconds ago
  * getRelativeTime(60); // minutes ago
  */
-function getRelativeTime(seconds) {}
+function getRelativeTime(seconds) {
+
+if (seconds >= 86400) return ("days ago");
+if (seconds >= 3600) return ("hours ago");
+if (seconds >= 60) return ("minutes ago");
+return ('seconds ago');
+}
 
 /**
  * Round to the nearest 100th decimal point.
@@ -35,7 +53,14 @@ function getRelativeTime(seconds) {}
  * roundToNearestHundredth(14.511); // 14.51
  * roundToNearestHundredth(14.499); // 14.5
  */
-function roundToNearestHundredth(num) {}
+function roundToNearestHundredth(num) {
+let newNum;
+
+newNum = (Math.trunc(num * 100))/100;
+if ((num - newNum ) > .004) newNum += .01;
+
+return newNum;
+}
 
 // Ignore this. It is for the tests.
 module.exports = {
