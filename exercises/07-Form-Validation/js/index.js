72d3a35088bd05ca3c8b34e0d8f2815a86fb5f3f
daveknussman <dknussman@yahoo.com>
2019-11-20 18:37:22 -0500

    form validation

--- exercises/07-Form-Validation/js/index.js
+++ exercises/07-Form-Validation/js/index.js
@@ -24,9 +24,16 @@
 
 // };
 
+let valName = '';
+let valBirthdate = '';
+let valGender = '';
+let valGuests = '';
+let valReg = '';
+
+
 const randomForm = document.querySelector("#form");
 randomForm.addEventListener("submit", event => {
-  event.preventDefault(); // Stops the page from refreshing
+  event.preventDefault(); // Stops the page from refreshing or being submitted
   let errorFlag = false;
    let inputs = Object.values(event.target.elements);
    // console.log(inputs);
@@ -38,7 +45,18 @@ randomForm.addEventListener("submit", event => {
             inputItem.classList.add('error');
             errorFlag = true;
          } else {
-            // console.log(inputItem.name + ' is ' + inputItem.value);
+            // console.log(inputItem);
+            switch (inputItem.name) {
+               case 'customerName':
+                  valName = inputItem.value;
+                  break;
+               case 'birthday':
+                  valBirthdate = inputItem.value;
+                  break;
+               case 'res_code':
+                  valReg = inputItem.value;
+                  break;
+            }
             if (inputItem.classList.contains("error")) {
                inputItem.classList.remove('error');
              }
@@ -47,24 +65,78 @@ randomForm.addEventListener("submit", event => {
       if (inputItem.tagName === 'SELECT') {
          if (inputItem.name === 'gender') {
             if (inputItem.value === 'GENDER') {
-               console.log(inputItem.name + ' is unselected');
+               // console.log(inputItem.name + ' is unselected');
+               inputItem.classList.add('error');
                errorFlag = true;
-            // } else {
-            //    console.log(inputItem.name + ' is ' + inputItem.value);           
-            }
+              // } else {
+            //    console.log(inputItem.name + ' is ' + inputItem.value);
+            } else {
+            // console.log(inputItem.name + ' is ' + inputItem.value);
+            valGender = inputItem.value;
+            if (inputItem.classList.contains("error")) {
+               inputItem.classList.remove('error');
+            }           
+         }
          }
          if (inputItem.name === 'class') {
             if (inputItem.value === 'How many Guest') {
-               console.log(inputItem.name + ' is unselected');
+               // console.log(inputItem.name + ' is unselected');
+               inputItem.classList.add('error');
                errorFlag = true;
-            // } else {
-            //    console.log(inputItem.name + ' is ' + inputItem.value);           
+            } else {
+               valGuests = inputItem.value;
+               // console.log(inputItem.name + ' is ' + inputItem.value);
+               if (inputItem.classList.contains("error")) {
+                  inputItem.classList.remove('error');
+               } 
             }
          }
       }
    }
-   if (!errorFlag) {
-      // alert('submitting');
-      document.getElementById("form").submit();
-   }
+   if (errorFlag) {
+      alert('Fix data entry errors');
+      return;
+   } else {
+      randomForm.style.display = 'none';
+      const box = document.querySelector('#box');
+      // this loop is incomplete - she waas going too fast so i did it in a hardcoded way
+      // elements.ForEach((element) => {
+      //    const paragraph = document.createElement('p');
+      //    paragraph.textContent = 'Name: ' + valName;
+      //    box.appendChild
+      //    // paragraph.textContent = `${element.getAttribute'()
+      // });
+      const paragraph1 = document.createElement('p');
+      const paragraph2 = document.createElement('p');
+      const paragraph3 = document.createElement('p');
+      const paragraph4 = document.createElement('p');
+      const paragraph5 = document.createElement('p');
+
+      paragraph1.textContent = 'Name: ' + valName;
+      box.appendChild(paragraph1);
+      paragraph2.textContent = 'Birthday: ' + valBirthdate;
+      box.appendChild(paragraph2);
+      paragraph3.textContent = 'Gender: ' + valGender;
+      box.appendChild(paragraph3);
+      paragraph4.textContent = 'Guests: ' + valGuests;
+      box.appendChild(paragraph4);
+      paragraph5.textContent = 'Reg Code: ' + valReg;
+      box.appendChild(paragraph5);
+
+      // alert(valName);
+      // alert(valBirthdate);
+      // alert(valGender);
+      // alert(valGuests);
+      // alert(valReg);
+
+      // dont want to submit
+      // document.getElementById("form").submit();
+   } 
 });
+
+
+   // valName = '';
+   // valBirthdate = '';
+   // valGender = '';
+   // valGuests = '';
+   // valReg = '';
